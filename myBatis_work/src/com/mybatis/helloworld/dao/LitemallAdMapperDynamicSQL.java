b3aeb602b8227fc803c3f1bd7adf975091698740
xuli309 <jswawa@qq.com>
2019-12-05 14:24:41 +0800

    https://github.com/xuli309/project.git

--- myBatis_work/src/com/mybatis/helloworld/dao/LitemallAdMapperDynamicSQL.java
+++ myBatis_work/src/com/mybatis/helloworld/dao/LitemallAdMapperDynamicSQL.java
@@ -2,6 +2,8 @@ package com.mybatis.helloworld.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mybatis.helloworld.bean.LitemallAd;
 
 public interface LitemallAdMapperDynamicSQL {
@@ -15,4 +17,12 @@ public interface LitemallAdMapperDynamicSQL {
 	public void updateLitemAdConditionSet(LitemallAd litemallAd); 
 	
 	public List<LitemallAd> getLitemAdByConditionChoose(LitemallAd litemallAd); 
+	
+	public List<LitemallAd> getLitemAdByIds(@Param("ids")List<Integer> ids); 
+	
+	//批量操作：修改 删除 增加
+	public void addAds(@Param("ads")List<LitemallAd> litemallAds);
+	public void deleteAds(@Param("ids")List<Integer> ids);
+	
+	public void updateAds(@Param("ads")List<LitemallAd> litemallAds);
 }
--- myBatis_work/src/com/mybatis/helloworld/test/MyBatisTestDynamicSQL.java
+++ myBatis_work/src/com/mybatis/helloworld/test/MyBatisTestDynamicSQL.java
@@ -2,6 +2,8 @@ package com.mybatis.helloworld.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -103,7 +105,7 @@ public class MyBatisTestDynamicSQL {
 			
 			LitemallAd litemallAd = new LitemallAd();
 			litemallAd.setId(12);
-			litemallAd.setName("合作");
+			litemallAd.setName("食品");
 			litemallAd.setContent("合作 食品");
 			litemallAd.setDeleted(0);
 			
@@ -115,6 +117,108 @@ public class MyBatisTestDynamicSQL {
 		}
 	}
 	
+	@Test
+	public void testForeach() throws Exception{
+		SqlSessionFactory ssf= getSqlSessionFactory();
+		
+		SqlSession session = ssf.openSession();// ssf.openSession(true) 自动提交
+		try{
+			//获取Mapper接口的代理实现类对象
+			LitemallAdMapperDynamicSQL mapper = session.getMapper(LitemallAdMapperDynamicSQL.class);
+			List<Integer> ids = new ArrayList<Integer>();
+			ids.add(1);
+			ids.add(2);
+			ids.add(3);
+			
+			List<LitemallAd> list = mapper.getLitemAdByIds(ids);
+			System.out.println(list);
+			
+		}finally{
+			session.close();
+		}
+	}
 	
+
+	@Test
+	public void testBatch() throws Exception{
+		SqlSessionFactory ssf= getSqlSessionFactory();
+		
+		SqlSession session = ssf.openSession();
+		try{
+			
+			LitemallAdMapperDynamicSQL mapper = session.getMapper(LitemallAdMapperDynamicSQL.class);
+			
+			//批量增加
+			List<LitemallAd> list = new ArrayList<LitemallAd>();
+			LitemallAd litemallAd0 = new LitemallAd(null, "aa", "", "http://yanxuan.nosdn.127.net/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg", 1,
+					"aa", null, null, 1, new Date(), new Date(), 0);
+			LitemallAd litemallAd1 = new LitemallAd(null, "bb", "", "http://yanxuan.nosdn.127.net/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg", 1,
+					"bb", null, null, 1, new Date(), new Date(), 0);
+			LitemallAd litemallAd2 = new LitemallAd(null, "cc", "", "http://yanxuan.nosdn.127.net/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg", 1,
+					"cc", null, null, 1, new Date(), new Date(), 0);
+			list.add(litemallAd0);
+			list.add(litemallAd1);
+			list.add(litemallAd2);
+			
+			mapper.addAds(list);
+			session.commit();
+		}finally{
+			session.close();
+		}
+	}
+	
+	
+	@Test
+	public void testBatchDelete() throws Exception{
+		SqlSessionFactory ssf= getSqlSessionFactory();
+		
+		SqlSession session = ssf.openSession();
+		try{
+			//批量删除
+			LitemallAdMapperDynamicSQL mapper = session.getMapper(LitemallAdMapperDynamicSQL.class);
+			List<Integer> ids = new ArrayList<Integer>();
+			ids.add(16);
+			ids.add(17);
+			ids.add(18);
+			
+			mapper.deleteAds(ids);
+			session.commit();
+			
+		}finally{
+			session.close();
+		}
+	}
+	
+	
+	@Test
+	public void testBatchUpdate() throws Exception{
+		SqlSessionFactory ssf= getSqlSessionFactory();
+		
+		SqlSession session = ssf.openSession();
+		try{
+			
+			LitemallAdMapperDynamicSQL mapper = session.getMapper(LitemallAdMapperDynamicSQL.class);
+			
+			//批量修改
+			List<LitemallAd> list = new ArrayList<LitemallAd>();
+			LitemallAd litemallAd0 = new LitemallAd();
+			litemallAd0.setId(12);
+			litemallAd0.setName("合作合作");
+			litemallAd0.setContent("合作合作");
+			
+			LitemallAd litemallAd1 = new LitemallAd();
+			litemallAd1.setId(15);
+			litemallAd1.setName("合作食品");
+			litemallAd1.setContent("合作食品");
+			
+			list.add(litemallAd0);
+			list.add(litemallAd1);
+			
+			mapper.updateAds(list);
+			session.commit();
+		}finally{
+			session.close();
+		}
+	}
 	
 }
